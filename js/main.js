ae77655fbf0ac9fe12f9c9be55d6b2c1ae7c9890
Elefly13 <elymara13@gmail.com>
2021-07-29 18:59:35 +0200

    fine esercizio

--- js/main.js
+++ js/main.js
@@ -11,30 +11,42 @@ while (myArrayNumeri.length < numI) {
     }  
 }
 
-alert("Hai 30 secondi per memorizzare i numeri: " + myArrayNumeri);
+alert("Memorizza i numeri: " + myArrayNumeri);
+console.log(myArrayNumeri);
 
 // Da li parte un timer di 30 secondi.
 
-let secondi = 5;
+let secondi = 2;// da inserire 30 secondi
 let idInterval = setInterval(timer, 1000);
 function timer(){
     document.getElementById("secondi").innerHTML = secondi;
     if (secondi == 0) {
         clearInterval(idInterval);
+        // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+        let arrayNumGiocatore = [];
+        for (let i = 0; i < 5; i++) {
+            arrayNumGiocatore.push(parseInt(prompt("inerisci i numeri uno alla volta")));
+        }
+        console.log(arrayNumGiocatore);
+
+        // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+        // questa soluzione non è corretta così compara solo se numero e posizione sono identici(da rivedere!)
+        if  (comparaArray(arrayNumGiocatore, myArrayNumeri) == true) {
+            console.log("hai vinto");
+        }  else {
+            console.log("hai perso");
+        }
+
+       
+    
     }
     secondi--;
 }
 
 
-let arrayNumGiocatore = [];
-    parseInt(prompt("Inserisci il primo numero"));
-    parseInt(prompt("Inserisci il secondo numero"));
-    parseInt(prompt("Inserisci il terzo numero"));
-    parseInt(prompt("Inserisci il quarto numero"));
-    parseInt(prompt("Inserisci il quinto numero"));
 
-// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
 
 
 
@@ -53,7 +65,14 @@ function existInArray (array, item) {
     }
     return false
 }
-
-
-
+//fuzione di comparazione di due array
+function comparaArray(array1, array2) {
+    if (array1.length != array2.length) return false;
+    for (var i = 0; i < array1.length; i++){
+        if (array1[i] != array2[i]) {
+            return false;
+        }
+    }
+    return true; 
+}
 // fine sezione FUNZIONI
